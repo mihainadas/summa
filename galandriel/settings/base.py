30b9b9c3226b91d2597f3329d2675856a5d2e59c
Mihai Nadăș <mihai@nadas.ro>
2023-12-03 11:13:53 +0200

    Replace admin URL and add core app

--- galandriel/settings/base.py
+++ galandriel/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galandriel.apps.MyAdminConfig', # replaced 'django.contrib.admin' with custom admin site
+    'core',
     'glife',
 
 ]
--- galandriel/urls.py
+++ galandriel/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('glife.urls')),
+    path('', include('core.urls')),
+    path('glife/', include('glife.urls')),
 ]
 
 if settings.DEBUG:
