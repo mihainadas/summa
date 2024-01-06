139a29a975e4c541484ed3c0da43c87e3bf90f8c
Mihai Nadăș <mihai@nadas.ro>
2024-01-06 07:42:38 +0200

    Update .dockerignore and .gitignore files

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@ set -e
 python manage.py migrate --noinput
 python manage.py core_create_superuser
 python manage.py core_load_summa
-python manage.py process_tasks > django-background-tasks.log &
+python manage.py process_tasks &
 exec gunicorn -w 4 -b 0.0.0.0:8000 galandriel.wsgi:application
